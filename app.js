12224f8d051db6c96ff7ba630c022ad797a3590f
Yuriy Glushchenko <yuriygl86@gmail.com>
2023-03-30 20:26:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('dqdqwdqd')
+
+function isPrimeNumber(x){
+    if(x==1){return false};
+    for (let i=2; i<x; i++){
+        if(x%i==0 || x==1){
+            return false
+        }
+    }
+    return true
+}
+
+function primesFinder(n){
+    let res = []
+    let primeNumber = 2
+    while(res.length<n){
+        
+        if(isPrimeNumber(primeNumber)){
+            res.push(primeNumber)
+        }
+        primeNumber++
+    }
+    return res
+}
+
+console.log(primesFinder(1000))
